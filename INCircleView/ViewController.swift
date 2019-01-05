93edd8daca7c01ba924b8cb8d7858c72a0c41beb
Igor Nakonetsnoi <igorest7@gmail.com>
2019-01-05 14:45:28 +0000

    Initial Commit

---
+++ INCircleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INCircleView
+//
+//  Created by Igor Nakonetsnoi on 05/01/2019.
+//  Copyright © 2019 Igor Nakonetsnoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
